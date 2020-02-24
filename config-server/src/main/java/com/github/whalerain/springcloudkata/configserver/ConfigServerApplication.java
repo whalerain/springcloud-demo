92be1df57cfebc649bbf1319b92a1ec00423c9cd
xi.zhang <xi.zhang@ai-research.cn>
2020-02-24 16:19:10 +0800

    update: 将config-server作为微服务注册
    1. 修改包名
    2. 增加eureka client配置

--- config-server/src/main/java/com/github/whalerain/springcloudkata/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/github/whalerain/springcloudkata/configserver/ConfigServerApplication.java
@@ -1,14 +1,16 @@
-package com.github.whalerain.springcloudkata;
+package com.github.whalerain.springcloudkata.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * 配置服务
  * @author ZhangXi
  */
 @EnableConfigServer
+@EnableEurekaClient
 @SpringBootApplication
 public class ConfigServerApplication {
 
