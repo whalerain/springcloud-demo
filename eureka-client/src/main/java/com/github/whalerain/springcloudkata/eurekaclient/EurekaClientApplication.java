eab9f5b749c8f9a9d0ea91b8bc821c7c6afa1919
xi.zhang <xi.zhang@ai-research.cn>
2020-02-24 14:59:45 +0800

    add: 初始化eureka-client项目

---
+++ eureka-client/src/main/java/com/github/whalerain/springcloudkata/eurekaclient/EurekaClientApplication.java
@@ -0,0 +1,19 @@
+package com.github.whalerain.springcloudkata.eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * eureka 微服务客户端
+ * @author ZhangXi
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+}
